d893d20ae3c1ebf3878513cea6fb6471cf50e776
zhouyan_a <3205047387@qq.com>
2025-05-21 14:27:07 +0800

    移动websocket工具位置

--- src/main/java/com/pet_science/utils/MyWebSocketHandler.java
+++ src/main/java/com/pet_science/utils/MyWebSocketHandler.java
@@ -1,10 +1,9 @@
-package com.pet_science.websocket;
+package com.pet_science.utils;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.pet_science.pojo.User;
 import com.pet_science.service.UserService;
-import com.pet_science.utils.JWTUtil;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
